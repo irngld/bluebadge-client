93d582eddb743bc1d2fb03b52882fd500d5d5ab0
irngld <iaingould1@gmail.com>
2021-05-27 23:04:20 -0400

    can delete favorites, css styling hamburger/menu

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -5,72 +5,76 @@ import Details from "./Details";
 import background from "../img/searchDrinksBackground.jpg";
 
 const Favorites = (props) => {
-    const [currentDrinkId, setCurrentDrinkId] = useState();
-    const [results, setResults] = useState();
-    const [ingredients, setIngredients] = useState();
-    const data = props;
-    const [showDetails, setShowDetails] = useState(false);
-    const [showSearch, setShowSearch] = useState(true);
-    
-    let getFavDrinks = 'http://localhost:5000/favorites/show';
-    const token = localStorage.getItem('token');
-    
-        
-    const fetcher = () => {
-        fetch(getFavDrinks, {
-            method: 'GET',
-            headers: new Headers({
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': token
-            })
-        })
-        .then(res => res.json())
-        .then(json => {
-            let drinks = json.favDrink?.map(drink => {
-                return {
-                    id: drink.id,
-                    userId: drink.userId,
-                    idDrink: drink.drinkId,
-                    strDrink: drink.drinkName,
-                    strDrinkThumb: drink.drinkThumb,
-                    rating: drink.rating,
-                    createdAt: drink.createdAt,
-                    updatedAt: drink.updatedAt
-                }
-            });
-            setResults(drinks)
-        })
-        .catch(err => {
-            console.log(err);
-        })
-    }
+  const [currentDrinkId, setCurrentDrinkId] = useState();
+  const [results, setResults] = useState();
+  const [ingredients, setIngredients] = useState();
+  const data = props;
+  const [showDetails, setShowDetails] = useState(false);
+  const [showSearch, setShowSearch] = useState(true);
 
-    useEffect(() => {
-        fetcher();
-    }, []);
-    
+  let getFavDrinks = "http://localhost:5000/favorites/show";
+  const token = localStorage.getItem("token");
 
-    return (
-        <div>
-            <p>FAVORITES HERE</p>
-            <Container className='d-flex flex-wrap'>
-            {results == null ? (
-                <p>No Favorites Found</p>
-            ) : (
-            results.map((obj, index) => {
-                return (
-                    <DrinkCard
-                        drink={obj}
-                        key={obj}
-                        // onSelect={}
-                    />
-                );
-            })
-            )}
-            </Container>
-        </div>
-    )
-}
+  const fetcher = () => {
+    fetch(getFavDrinks, {
+      method: "GET",
+      headers: new Headers({
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: token,
+      }),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        let drinks = json.favDrink?.map((drink) => {
+          return {
+            id: drink.id,
+            userId: drink.userId,
+            idDrink: drink.drinkId,
+            strDrink: drink.drinkName,
+            strDrinkThumb: drink.drinkThumb,
+            rating: drink.rating,
+            createdAt: drink.createdAt,
+            updatedAt: drink.updatedAt,
+            isFavorite: true,
+          };
+        });
+        setResults(drinks);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const itemUpdated = () => {
+    fetcher();
+  };
+
+  useEffect(() => {
+    fetcher();
+  }, []);
+
+  return (
+    <div>
+      <p>FAVORITES HERE</p>
+      <Container className="d-flex flex-wrap">
+        {results == null ? (
+          <p>No Favorites Found</p>
+        ) : (
+          results.map((obj, index) => {
+            return (
+              <DrinkCard
+                drink={obj}
+                key={obj}
+                // onSelect={}
+                onUpdate={itemUpdated}
+              />
+            );
+          })
+        )}
+      </Container>
+    </div>
+  );
+};
 
 export default Favorites;
--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -1,16 +1,31 @@
-import { slide as Menu } from 'react-burger-menu';
+import { Link } from "react-router-dom";
+import { clearToken } from "./Auth/UserToken";
+import { slide as Menu } from "react-burger-menu";
 
 import "./hamburger.css";
 
 const HamburgerMenu = () => {
-    return (
-        <Menu>
-          <a id="home" className="menu-item" href="/home">Home</a>
+  return (
+    <Menu>
+      {/* <a id="home" className="menu-item" href="/home">Home</a>
           <a id="search" className="menu-item" href="/searchdrinks">Search Drinks</a>
           <a id="random" className="menu-item" href="/random">Random</a>
-          <a id="favorites" className="menu-item" href="/favorites">Favorites</a>
-        </Menu>
-      );
-}
+          <a id="favorites" className="menu-item" href="/favorites">Favorites</a> */}
+      <div className="menu-item">
+        <Link to="/home">Home</Link>
+
+        <Link to="/searchdrinks">Search Drinks</Link>
+
+        <Link to="/random">Random</Link>
+
+        <Link to="/favorites">Favorites</Link>
+
+        <Link to="/" onClick={clearToken}>
+          Logout
+        </Link>
+      </div>
+    </Menu>
+  );
+};
 
 export default HamburgerMenu;
