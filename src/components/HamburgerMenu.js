eb6359987ba6042678e8470adc44645e0cd2e5bf
jackiejones3bb <jackiejones3bb@gmail.com>
2021-05-27 21:49:44 -0400

    more styling

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -1,16 +1,28 @@
-import { slide as Menu } from 'react-burger-menu';
+import { slide as Menu } from "react-burger-menu";
+import { clearToken } from "./Auth/UserToken";
 
 import "./hamburger.css";
 
 const HamburgerMenu = () => {
-    return (
-        <Menu>
-          <a id="home" className="menu-item" href="/home">Home</a>
-          <a id="search" className="menu-item" href="/searchdrinks">Search Drinks</a>
-          <a id="random" className="menu-item" href="/random">Random</a>
-          <a id="favorites" className="menu-item" href="/favorites">Favorites</a>
-        </Menu>
-      );
-}
+  return (
+    <Menu className='hamburger'>
+      <a id='home' className='menu-item' href='/home'>
+        Home
+      </a>
+      <a id='search' className='menu-item' href='/searchdrinks'>
+        Search Drinks
+      </a>
+      <a id='random' className='menu-item' href='/random'>
+        Random
+      </a>
+      <a id='favorites' className='menu-item' href='/favorites'>
+        Favorites
+      </a>
+      <a id='logout' className='menu-item' href='/' onClick={clearToken}>
+        Logout
+      </a>
+    </Menu>
+  );
+};
 
 export default HamburgerMenu;
